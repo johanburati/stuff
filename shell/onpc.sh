0bf577dc6a4cc3f0e58fd4956345f2dde3b54f25
Johan Burati <johan.burati@gmail.com>
2011-06-15 17:01:42 +0900

    Update onpc.sh script

--- shell/onpc.sh
+++ shell/onpc.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
-url=http://on-n-est-pas-couche.france2.fr/IMG/asx/onpc-101.asx
-video=$(curl -s $url |  perl -lane 'if (/href="(.*\.asx)"/) { print $1}')
-vlc ${url}${video} 2>&1 >/dev/null &
+#
+# I use this script to watch a french tv show with vlc .
+#
+# Ce script extrait l'URL MMS pour l'e'mission courante d' "On n'est pas couche'"
+# et lance VLC.
+#
+# Johan Burati (johan.burati@gmail.com)
+# Code is licensed under GNU GPL license.
+#
+
+url=http://on-n-est-pas-couche.france2.fr/
+# Get the current emission
+current=$(curl -s $url | perl -lane 'if (/href="(\S.*\.asx)"/) { print $1}')
+# Extract the video feed
+video=$(curl -s $url$current |  perl -lane 'if (/href="(mms:\S.*\.wmv)"/) { print $1}')
+# Startup VLC
+vlc ${video} 2>&1 >/dev/null &
