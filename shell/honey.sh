f478f34935c5d1bbbd0f3c068f386de8c12ab820
Johan Burati <johan.burati@gmail.com>
2011-06-16 11:27:40 +0900

    Add honey.sh, a simple honeypot

---
+++ shell/honey.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Simple httpd honey
+#
+# Johan Burati (johan.burati@gmail.com)
+# Code is licensed under GNU GPL license.
+#
+netcat=$(which nc 2>/dev/null)
+honey_port=80
+honey_log=$HOME/tmp/honey.log
+honey_lock=$HOME/tmp/honey.lock
+honey_banner="MS-IIS WEB SERVER 5.0\r"
+
+loop() {
+    while [ -f ${honey_lock} ]; do
+        $netcat -lp $honey_port -c 'echo HTTP/1.0 200 OK' 2>&1 >> ${honey_log}
+        echo "`date +'%Y-%m-%d %H:%M'` Attempted connection on honey_port $honey_port" >> ${honey_log}
+        echo "-------------------------------------------" >> ${honey_log}
+    done
+}
+
+quit() {
+  echo "<Ctrl-C> quitting ..."
+  rm ${honey_lock}
+  exit 0
+}
+
+trap 'quit' 2
+if [ ! -x "$netcat" ]; then
+   echo "Oops, looks like netcat is not installed on your system !"
+   exit -1
+fi
+echo "Starting honey on honey_port $honey_port, hit <Ctrl-C> to quit..."
+touch ${honey_lock}
+loop
